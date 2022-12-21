fdc5e81daf681c73c61e5232febb073c9f82d596
Jorge Perdigao <jorge@perdigao.org>
2022-12-21 18:48:05 +0100

    stuck in 11 part 2

--- 11/solve.py
+++ 11/solve.py
@@ -98,10 +98,10 @@ def do_monkey_round(monkeys, very_worried=False):
     return monkeys
 
 
-monkeys = read_monkey_setup(monkey_setup)
+
 print("########")
 
-
+monkeys = read_monkey_setup(monkey_setup)
 for round_index in range(20):
     #print("### Round",round_index+1,"###")
     monkeys = do_monkey_round(monkeys)
@@ -117,10 +117,85 @@ print("Part 1:", monkey_scores[0]*monkey_scores[1])
 #### second part
 
 # OMG the numbers are exploding!! I have to do something about that
-for round_index in range(10000):
-    monkeys = do_monkey_round(monkeys,very_worried=True)
+
+# for round_index in range(1000):
+#     monkeys = do_monkey_round(monkeys,very_worried=True)
+#     print("### Round",round_index+1,"###")
+#     if True: #if (round_index % 1000) == 0:
+#         print("### Round",round_index+1,"###")
+#         for m, monkey in enumerate(monkeys):
+#             print("Monkey",m,":",monkey["inspect_count"])#,monkey["items"])
+
+import math
+
+# muito ineficiente... vamos ver se chega
+def find_prime_facs(n):
+  list_of_factors=[]
+  i=2
+  while n>1:
+    if n%i==0:
+      list_of_factors.append(i)
+      n=n/i
+      i=i-1
+    i+=1  
+  return list_of_factors
+
+def change_items_to_factors(monkeys):
+    for monkey in monkeys:
+        for i, item in enumerate(monkey["items"]):
+            monkey["items"][i] = find_prime_facs(item)
+    return monkeys
+
+def do_monkey_round_2(monkeys):
+    for m, monkey in enumerate(monkeys):
+        while monkey["items"]:
+            monkey["inspect_count"]+=1
+            item = monkey["items"].pop()
+            #print("  Monkey inspects an item with a worry level of ",item)
+            #print(monkey["op"])
+            #_locals = {"old": item}
+            #exec(monkey["op"], {}, _locals)
+            #new_item = _locals["new"]
+            op=monkey["op"]
+            if(op=="new = old * old"):
+                new_item=item+item
+            elif op[10]=="*":
+                new_item=item+[int(op[11:])]
+            elif op[10]=="+":
+                # addition  will break this solution :(
+                new_item=find_prime_facs(math.prod(item)+int(op[11:]))
+            else:
+                print("you did something wrong!")
+                exit(0)
+            #print("  New worry level of item is ",new_item)
+            next_monkey = -1
+            if (monkey["divby"] in new_item):
+                next_monkey = monkey["tmonkey"]
+            else:
+                next_monkey = monkey["fmonkey"]
+            #print("  Next monkey is:",next_monkey)
+            if "next_items" in monkeys[next_monkey].keys():
+                monkeys[next_monkey]["next_items"].append(new_item)
+            else:
+                monkeys[next_monkey]["next_items"] = [new_item]
+        for monkey in monkeys:
+            if "next_items" in monkey.keys():
+                monkey["items"] += monkey["next_items"]
+                monkey.pop('next_items', None)
+    return monkeys
+
+monkeys = read_monkey_setup(monkey_setup)
+#monkeys = change_items_to_factors(monkeys)
+print(monkeys)
+for round_index in range(1000):
     print("### Round",round_index+1,"###")
-    if True: #if (round_index % 1000) == 0:
-        print("### Round",round_index+1,"###")
-        for m, monkey in enumerate(monkeys):
-            print("Monkey",m,":",monkey["inspect_count"],monkey["items"])
+    #monkeys = do_monkey_round_2(monkeys)
+    monkeys = do_monkey_round(monkeys,very_worried=True)
+    for m, monkey in enumerate(monkeys):
+        print("Monkey",m,":",monkey["inspect_count"])
+        pass
+
+# monkey_scores = sorted([monkey["inspect_count"] for monkey in monkeys])
+# print(monkey_scores)
+# monkey_scores.reverse()
+# print("Part 2:", monkey_scores[0]*monkey_scores[1])
